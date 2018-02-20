2039bae64e039eb4dcc2c458d6b05b6df6914878
Econ Account <ouecon002@schooner1.oscer.ou.edu>
2018-02-20 13:03:56 -0600

    Completed PS5

---
+++ ProblemSets/PS5/PS5_wu.R
@@ -0,0 +1,19 @@
+webpage1 <- read_html("https://www.forbes.com/sites/games/2017/12/22/the-best-video-games-of-2017/#5804abb5696f")
+webpage1
+results1 <- webpage1 %>% html_nodes("u")
+results1 <- results1[-11]  ### This data should not in the result.
+results1
+records1 <- vector("list", length = length(results1))
+records1
+
+for (i in 1:20) {
+  
+  game <- str_c(results1[i] %>% html_nodes("em") %>% html_text(trim = TRUE))
+  company <- str_c(xml_contents(results1[i])[2])
+  records1[[i]] <- data_frame(game=game, company=company)
+  
+}
+
+df <- bind_rows(records1)
+df
+
