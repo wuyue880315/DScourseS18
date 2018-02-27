42f88d8febd22c32eab87cda4c22d0e2b3ef9a27
Econ Account <ouecon002@schooner3.oscer.ou.edu>
2018-02-27 16:12:24 -0600

    completed PS5

--- ProblemSets/PS5/PS5_wu.R
+++ ProblemSets/PS5/PS5_wu.R
@@ -1,3 +1,8 @@
+####################
+# Data without api #
+####################
+
+
 webpage1 <- read_html("https://www.forbes.com/sites/games/2017/12/22/the-best-video-games-of-2017/#5804abb5696f")
 webpage1
 results1 <- webpage1 %>% html_nodes("u")
@@ -16,4 +21,28 @@ for (i in 1:20) {
 
 df <- bind_rows(records1)
 df
+
+write_csv(df,"game & company")
+
+
+#################
+# data with api #
+#################
+
+library(httr)
+library(jsonlite)
+library(lubridate)
+gameT<-GET(url="https://api.twitch.tv/kraken/games/top")
+gameT
+names(gameT)
+content(gameT)
+head(gameT)
+data_frame(gameT)
+gameTex<-rawToChar(gameT$content)
+nchar(gameTex)
+gamejson<-fromJSON(gameTex)
+class(gamejson)
+length(gamejson)
+gamejson[[1]]
+gamejson
 
